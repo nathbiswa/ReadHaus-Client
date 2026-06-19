10182aee285480ea0319f5bfafab264b42e6925f
nathbiswa <biswanroy29@gmail.com>
2026-06-19 12:18:52 +0600

    stripe payment and success page setup done

--- src/app/api/payments/route.js
+++ src/app/api/payments/route.js
@@ -33,7 +33,7 @@ export async function POST() {
 
             },
             mode: 'subscription',
-            success_url: `${origin}/payments/success?session_id={CHECKOUT_SESSION_ID}`,
+            success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
         });
         return NextResponse.redirect(session.url, 303)
     } catch (err) {
